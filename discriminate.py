8f8a837c36843f72cdf6132f4082aad284fb2ccb
Shuheng Johnny Liu <wish1104@outlook.com>
2018-03-28 09:28:10 +0800

    deleted the model defined in discriminate.py, allowing for setting netD.eval() on and off

--- discriminate.py
+++ discriminate.py
@@ -35,6 +35,7 @@ parser.add_argument('--outf', default='.', help='folder to output images and mod
 parser.add_argument('--dropoutD', default=None, type=float, help='implements dropout in netD')
 parser.add_argument('--dropoutG', default=None, type=float, help='implements dropout in netG')
 parser.add_argument('--manualSeed', type=int, help='manual seed')
+parser.add_argument('--eval', action='store_true', help='whether to set netD.eval() while evaluating')
 
 opt = parser.parse_args()
 print(opt)
@@ -93,83 +94,16 @@ ngpu = int(opt.ngpu)
 ndf = int(opt.ndf)
 nc = 3
 
-
-# custom weights initialization called on netG and netD
-# def weights_init(m):
-#     classname = m.__class__.__name__
-#     if classname.find('Conv') != -1:
-#         m.weight.data.normal_(0.0, 0.02)
-#     elif classname.find('BatchNorm') != -1:
-#         m.weight.data.normal_(1.0, 0.02)
-#         m.bias.data.fill_(0)
-
-#
-# class _netD(nn.Module):
-#     def __init__(self, ngpu):
-#         super(_netD, self).__init__()
-#         self.ngpu = ngpu
-#         if opt.dropoutD:
-#             self.main = nn.Sequential(
-#                 # input is (nc) x 64 x 64
-#                 nn.Conv2d(nc, ndf, 4, 2, 1, bias=False),
-#                 nn.Dropout2d(p=opt.dropoutD, inplace=True), # Comment out to skip dropout
-#                 nn.LeakyReLU(0.2, inplace=True),
-#                 # state size. (ndf) x 32 x 32
-#                 nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
-#                 nn.Dropout2d(p=opt.dropoutD, inplace=True), # Comment out to skip dropout
-#                 nn.BatchNorm2d(ndf * 2),
-#                 nn.LeakyReLU(0.2, inplace=True),
-#                 # state size. (ndf*2) x 16 x 16
-#                 nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
-#                 nn.Dropout2d(p=opt.dropoutD, inplace=True), # Comment out to skip dropout
-#                 nn.BatchNorm2d(ndf * 4),
-#                 nn.LeakyReLU(0.2, inplace=True),
-#                 # state size. (ndf*4) x 8 x 8
-#                 nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False),
-#                 nn.Dropout2d(p=opt.dropoutD, inplace=True), # Comment out to skip dropout
-#                 nn.BatchNorm2d(ndf * 8),
-#                 nn.LeakyReLU(0.2, inplace=True),
-#                 # state size. (ndf*8) x 4 x 4
-#                 nn.Conv2d(ndf * 8, 1, 4, 1, 0, bias=False),
-#                 # nn.Dropout2d(p=opt.dropoutD, inplace=True), # Comment out to skip dropout
-#                 nn.Sigmoid()
-#             )
-#         else:
-#             self.main = nn.Sequential(
-#                 # input is (nc) x 64 x 64
-#                 nn.Conv2d(nc, ndf, 4, 2, 1, bias=False),
-#                 nn.LeakyReLU(0.2, inplace=True),
-#                 # state size. (ndf) x 32 x 32
-#                 nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
-#                 nn.BatchNorm2d(ndf * 2),
-#                 nn.LeakyReLU(0.2, inplace=True),
-#                 # state size. (ndf*2) x 16 x 16
-#                 nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
-#                 nn.BatchNorm2d(ndf * 4),
-#                 nn.LeakyReLU(0.2, inplace=True),
-#                 # state size. (ndf*4) x 8 x 8
-#                 nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False),
-#                 nn.BatchNorm2d(ndf * 8),
-#                 nn.LeakyReLU(0.2, inplace=True),
-#                 # state size. (ndf*8) x 4 x 4
-#                 nn.Conv2d(ndf * 8, 1, 4, 1, 0, bias=False),
-#                 nn.Sigmoid()
-#             )
-#
-#     def forward(self, input):
-#         if isinstance(input.data, torch.cuda.FloatTensor) and self.ngpu > 1:
-#             output = nn.parallel.data_parallel(self.main, input, range(self.ngpu))
-#         else:
-#             output = self.main(input)
-#
-#         return output.view(-1, 1).squeeze(1)
-
-
 netD = _netD(ngpu, dropout=opt.dropoutD, ndf=ndf, nc=nc)
 netD.apply(weights_init)
 if opt.netD != '':
     netD.load_state_dict(torch.load(opt.netD))
-netD.eval() # This is important, since modules are initialized to train mode and will behave ill on dropout and BN
+if opt.eval:
+    netD.eval() # This is important, since modules are initialized to train mode and will behave ill on dropout
+    print("model D IS set to eval")
+else:
+    print("model D is NOT set to eval")
+# and BN
 print(netD)
 
 
