5bb67e2e685686c2867118232af94ac0e302a98c
Shuheng Johnny Liu <wish1104@outlook.com>
2018-10-19 21:03:12 +0800

    modified prompt frequency

--- generate.py
+++ generate.py
@@ -12,10 +12,10 @@ parser.add_argument('--cuda', action='store_true', help='enables cuda')
 parser.add_argument('--ngpu', type=int, default=0, help='number of GPUs to use')
 parser.add_argument('--nz', type=int, default=100, help='size of the latent z vector')
 parser.add_argument('--ngf', type=int, default=64)
-parser.add_argument('--nsample', type = int, default=20, help='number of samples to be generated')
+parser.add_argument('--nsample', type=int, default=20, help='number of samples to be generated')
 parser.add_argument('--dropoutG', type=float, default=None, help='implements dropout in netG')
 parser.add_argument('--batchSize', type=int, default=1, help='size of batch')
-parser.add_argument('--eval', action='store_true', help = 'set G model to eval mode rather than train mode')
+parser.add_argument('--eval', action='store_true', help='set G model to eval mode rather than train mode')
 opt = parser.parse_args()
 print(opt)
 
@@ -38,9 +38,9 @@ nsample = int(opt.nsample)
 
 # Generate An Image from input json or default parameters
 for index in range(nsample):
-    if index % 100 == 0:
+    if index % 10 == 0:
         print("%4d images of %d generated" % (index, nsample))
-    Generator = DCGAN(netG=opt.netG, zvector=zvector, batchSize=batchSize, nz = opt.nz, ngf = opt.ngf,
+    Generator = DCGAN(netG=opt.netG, zvector=zvector, batchSize=batchSize, nz=opt.nz, ngf=opt.ngf,
                       outf=outf, cuda=cuda, ngpu=ngpu, dropoutG=opt.dropoutG)
     Generator.build_model()
     if opt.eval:
