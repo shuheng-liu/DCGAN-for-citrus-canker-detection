7b9776c9e4fc88c2d3e5b09ad68c30de529e74e4
Shuheng Johnny Liu <wish1104@outlook.com>
2018-10-19 21:04:21 +0800

    style change

--- crop_down.py
+++ crop_down.py
@@ -1,12 +1,14 @@
-import PIL
-import os, sys
+import os
 from PIL import Image
 
-def handle_folder(dir_raw, dir_new, grid_size = 64, margin_size = 2, max_imgs = -1):
+
+def handle_folder(dir_raw, dir_new, grid_size=64, margin_size=2, max_imgs=-1):
     assert os.path.isdir(dir_raw)
     legal_suffices = ['jpg', 'JPG', 'png', 'PNG']
-    try: os.mkdir(dir_new)
-    except: pass
+    try:
+        os.mkdir(dir_new)
+    except:
+        pass
     img_names = os.listdir(dir_raw)
     img_names.sort()
     img_count = 0
@@ -23,13 +25,14 @@ def handle_folder(dir_raw, dir_new, grid_size = 64, margin_size = 2, max_imgs = 
         num_h = (height - margin_size) // (grid_size + margin_size)
         for i in range(num_w):
             for j in range(num_h):
-                x0, y0 = margin_size + i * (grid_size + margin_size),  margin_size + j * (grid_size + margin_size)
+                x0, y0 = margin_size + i * (grid_size + margin_size), margin_size + j * (grid_size + margin_size)
                 x1, y1 = x0 + grid_size, y0 + grid_size
                 grid = img.crop([x0, y0, x1, y1])
-                save_name = os.path.splitext(img_name)[0] + "_%d_%d"%(i,j) + os.path.splitext(img_name)[1]
+                save_name = os.path.splitext(img_name)[0] + "_%d_%d" % (i, j) + os.path.splitext(img_name)[1]
                 grid.save(os.path.join(dir_new, save_name))
 
-def make_list(dir, txt_dir = '/Users/liushuheng/Desktop'):
+
+def make_list(dir, txt_dir='/Users/liushuheng/Desktop'):
     assert os.path.isdir(dir)
     assert os.path.isdir(txt_dir)
     legal_suffices = ['jpg', 'JPG', 'png', 'PNG']
@@ -44,11 +47,14 @@ def make_list(dir, txt_dir = '/Users/liushuheng/Desktop'):
                 tag = 1
             f.write('{} {}\n'.format(img_name, tag))
 
-def resize_real(dir_raw, dir_new, grid_size = 64):
+
+def resize_real(dir_raw, dir_new, grid_size=64):
     assert os.path.isdir(dir_raw)
     legal_suffices = ['jpg', 'JPG', 'png', 'PNG']
-    try: os.mkdir(dir_new)
-    except: pass
+    try:
+        os.mkdir(dir_new)
+    except:
+        pass
     img_names = os.listdir(dir_raw)
     img_names.sort()
     for img_name in img_names:
@@ -60,13 +66,6 @@ def resize_real(dir_raw, dir_new, grid_size = 64):
 
 
 if __name__ == "__main__":
-    paths = ['/Users/liushuheng/Desktop/dcgan-187',
-             '/Users/liushuheng/Desktop/dcgan-188',
-             '/Users/liushuheng/Desktop/dcgan-193',
-             '/Users/liushuheng/Desktop/dcgan-194']
-    real_path = '/Users/liushuheng/Documents/Computer Vision 2/TestPicture/PositiveSamplesHQ'
-    for path in paths:
-        grid_path = path + '-grid'
-        handle_folder(path, grid_path, max_imgs=10)
-        resize_real(real_path, grid_path)
-        make_list(grid_path)
+    path = '/uncropped'
+    grid_path = '/output'
+    handle_folder(path, grid_path, max_imgs=-1)
